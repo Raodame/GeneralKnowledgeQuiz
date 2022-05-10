62b0c6ee79f47d972deabb83d0aa1f2b6d2ca350
Raodame <asherkim04@gmail.com>
2022-05-10 17:08:40 -0500

    - Finished title window

--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,16 +7,18 @@ public class Window {
 
     JFrame window;
     Container con;
-    JPanel titleNamePanel, startButtonPanel;
+    JPanel titleNamePanel, mainButtonPanel, mainTextPanel;
     JLabel titleNameLabel;
-    Font titleFont = new Font("Times New Roman", Font.PLAIN,100);
+    Font titleFont = new Font("Times New Roman", Font.PLAIN,75);
     Font normalFont = new Font("Times New Roman", Font.PLAIN,30);
-    JButton simpleButton;
-    ButtonClick simpleButtonClick;
+    JButton simpleButton, myersButton;
+    JTextArea mainTextArea;
+    ButtonClick simpleButtonClick, myersButtonClick;
 
     public static void main(String[] args) {
 
         new Window();
+
     }
 
     public Window(){
@@ -37,32 +37,90 @@ public class Window {
         titleNameLabel.setForeground(Color.BLACK);
         titleNameLabel.setFont(titleFont);
 
-        startButtonPanel = new JPanel();
-        startButtonPanel.setBounds(200,300,400,200);
-        startButtonPanel.setBackground(Color.WHITE);
+        mainButtonPanel = new JPanel();
+        mainButtonPanel.setBounds(200,300,400,200);
+        mainButtonPanel.setBackground(Color.WHITE);
 
         simpleButton = new JButton("Simple Quiz");
         simpleButton.setBackground(Color.black);
         simpleButton.setForeground(Color.BLUE);
         simpleButton.setFont(normalFont);
+        simpleButton.setFocusPainted(false);
+        simpleButton.setActionCommand("simpleQuiz");
 
         simpleButtonClick = new ButtonClick();
         simpleButton.addActionListener(simpleButtonClick);
 
+        myersButton = new JButton("MBTI");
+        myersButton.setBackground(Color.black);
+        myersButton.setForeground(Color.BLUE);
+        myersButton.setFont(normalFont);
+        myersButton.setFocusPainted(false);
+        myersButton.setActionCommand("myersQuiz");
+
+        myersButtonClick = new ButtonClick();
+        myersButton.addActionListener(myersButtonClick);
+
         titleNamePanel.add(titleNameLabel);
-        startButtonPanel.add(simpleButton);
+        mainButtonPanel.add(simpleButton);
+        mainButtonPanel.add(myersButton);
 
         con.add(titleNamePanel);
-        con.add(startButtonPanel);
+        con.add(mainButtonPanel);
         window.setVisible(true); //must always be last line of constructor
 
+    }
+
+    public void createSimpleQuizScreen(){
+
+        titleNamePanel.setVisible(false);
+        mainButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100,100,600,250);
+        mainTextPanel.setBackground(Color.BLUE);
+        con.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("What color is a banana?");
+        mainTextArea.setBounds(100,100,600,250);
+        mainTextArea.setBackground(Color.BLACK);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
+    }
+
+    public void createMyersQuizScreen() {
+
+        titleNamePanel.setVisible(false);
+        mainButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.BLUE);
+        mainTextPanel.setLayout(new GridLayout(2,1));
+        con.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("This is the Myers text.");
+        mainTextArea.setBounds(100, 100, 600, 250);
+        mainTextArea.setBackground(Color.BLACK);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
     }
 
     private class ButtonClick implements ActionListener{
 
         public void actionPerformed(ActionEvent event){
-            Quiz.basic();
-        }
 
+            String yourChoice = event.getActionCommand();
+
+            if (yourChoice.equals("simpleQuiz"))
+                createSimpleQuizScreen();
+
+            if (yourChoice.equals("myersQuiz"))
+                createMyersQuizScreen();
+        }
     }
 }
